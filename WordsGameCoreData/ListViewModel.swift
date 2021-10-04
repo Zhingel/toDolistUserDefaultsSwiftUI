31876b07928c789171380b449574693b45ef7ce9
Stas <staszhingel@icloud.com>
2021-10-04 22:08:12 +0300

    refactoring

--- WordsGameCoreData/ListViewModel.swift
+++ WordsGameCoreData/ListViewModel.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
@@ -16,11 +21,12 @@ class ListViewModel: ObservableObject {
     
     
     func getItems() {
-        let newItems = [
-        ItemModel(title: "This is the first title", isCompleted: false),
-        ItemModel(title: "This is the second", isCompleted: true),
-        ItemModel(title: "Third title", isCompleted: false)]
-        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else {return}
+        self.items = savedItems
+        
     }
     func deleteItem(indexSet: IndexSet) {
         items.remove(atOffsets: indexSet)
@@ -44,6 +50,8 @@ class ListViewModel: ObservableObject {
     }
     
     func saveItems() {
-        UserDefaults.standard. set
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
+        }
     }
 }
